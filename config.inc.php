bd668d288a5c38c157a01e9f6ca7231242dfc83c
1569501393 <1569501393@qq.com>
2017-11-16 11:35:22 +0800

    初始化

---
+++ config.inc.php
@@ -0,0 +1,11 @@
+<?php
+return array (
+  'DB_HOST' => '192.168.1.55',
+  'DB_NAME' => 'mobilecms',
+  'DB_PWD' => 'PassWord',
+  'DB_USER' => 'root',
+  'DB_PORT' => '3306',
+  'DB_PREFIX' => 'cms_',
+  'DEFAULT_THEME' => 'default',
+); 
+?>
---
+++ data/install.temp.php
@@ -0,0 +1,13 @@
+<?php
+return array (
+  'db_host' => '192.168.1.55',
+  'db_port' => '3306',
+  'db_user' => 'root',
+  'db_pass' => 'PassWord',
+  'db_name' => 'mobilecms',
+  'db_prefix' => 'cms_',
+  'admin_user' => 'admin',
+  'admin_pass' => 'admin888',
+  'admin_email' => 'admin@qq.com',
+); 
+?>
